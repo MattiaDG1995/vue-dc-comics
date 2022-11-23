fb5c703394f33ae8925778e6bdb7d63bfd7dc36d
MattiaDG1995 <mattiadigennaro95@gmail.com>
2022-11-23 15:20:26 +0100

    main: sect content goes

--- src/components/MainComp.vue
+++ src/components/MainComp.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
-
+    <div class="sect">
+        <div class="container">
+            <span>Content goes here</span>
+        </div>
     </div>
 </template>
 
@@ -12,4 +14,15 @@
 
 <style lang="scss" scoped>
 
+    .sect{
+        background-color: black;
+        color: white;
+    }
+
+    .container{
+        width: 80%;
+        margin: 0 auto;
+        padding-top: 60px;
+        padding-bottom: 60px;
+    }
 </style>
